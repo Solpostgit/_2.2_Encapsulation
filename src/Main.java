0656536d94c71aef52fe3b7e6b3e7c92d41134dc
Solpostgit <begemot_off@mail.ru>
2023-10-04 22:52:57 +0300

    Создал класс методов PrintService.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import javax.print.PrintService;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Hogwarts[] printedStudents = new Hogwarts[]{
+        Griffindor harryPotter = new Griffindor("Harry Potter", "Griffindor", 7, 6, 5, 6, 7);
+        Griffindor hermioneGranger = new Griffindor("Hermione Granger", "Griffindor", 5, 5, 5, 5, 6);
+        Griffindor ronaldWeasley = new Griffindor("Ronald Weasley", "Griffindor", 4, 6, 3, 6, 5);
+
+        Hufflepuff zachariahSmith = new Hufflepuff("Zachariah Smith", "Hufflepuff", 4, 5, 6, 7, 6);
+        Hufflepuff cedricDiggory = new Hufflepuff("Cedric Diggory", "Hufflepuff", 5, 4, 4, 5, 7);
+        Hufflepuff justinFinchFletchley = new Hufflepuff("Justin Finch-Fletchley", "Hufflepuff", 6, 4, 7, 5, 5);
+
+        Ravenclaw zhouChang = new Ravenclaw("Zhou Chang", "Ravenclaw", 5, 5, 7, 5, 5, 7);
+        Ravenclaw padmaPatil = new Ravenclaw("Padma Patil", "Ravenclaw", 4, 6, 6, 7, 5, 5);
+        Ravenclaw marcusBelby = new Ravenclaw("Marcus Belby", "Ravenclaw", 6, 5, 6, 5, 7, 6);
+
+        Slytherin dracoMalfoy = new Slytherin("Draco Malfoy", "Slytherin", 6, 6, 5, 5, 7, 4, 7);
+        Slytherin grahamMontague = new Slytherin("Graham Montague", "Slytherin", 4, 5, 6, 5, 4, 5, 4);
+        Slytherin gregoryGoyle = new Slytherin("Gregory Goyle", "Slytherin", 5, 4, 4, 4, 5, 4, 5);
+
+        PrintService printService = new PrintService();
+        printService.printStud(harryPotter);
+        /*Hogwarts[] printedStudents = new Hogwarts[]{
                 new Griffindor("Harry Potter", "Griffindor", 7,6, 5, 6, 7),
                 new Griffindor("Hermione Granger", "Griffindor", 5,5, 5, 5, 6),
                 new Griffindor("Ronald Weasley", "Griffindor", 4,6, 3, 6, 5),
@@ -14,9 +33,8 @@ public class Main {
                 new Ravenclaw("Marcus Belby", "Ravenclaw", 6, 5, 6, 5, 7, 6),
                 new Slytherin("Draco Malfoy", "Slytherin", 6,6, 5, 5, 7,4, 7),
                 new Slytherin("Graham Montague", "Slytherin", 4,5, 6, 5, 4,5, 4),
-                new Slytherin("Gregory Goyle", "Slytherin", 5,4, 4, 4, 5,4, 5),
-        };
-        System.out.println(Arrays.toString(printedStudents));
-    }
+                new Slytherin("Gregory Goyle", "Slytherin", 5,4, 4, 4, 5, 4, 5)
+        } */
 
+    }
 }
