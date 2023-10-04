bf251fb4e845e32dee585afbab1b6cf218781186
Solpostgit <begemot_off@mail.ru>
2023-10-04 20:40:14 +0300

    Добавил класс Hufflepuff. Внёс поля и методы.

---
+++ src/Hufflepuff.java
@@ -0,0 +1,35 @@
+class Hufflepuff extends Hogwarts {
+    private int labour; //труд
+    private int loyalty; //верность
+    private int honesty; //честность
+    public Hufflepuff(String name, String faculty, int powerMagic, int transgresDist, int labour, int loyalty, int honesty) {
+        super(name, faculty, powerMagic, transgresDist);
+        this.labour = labour;
+        this.loyalty = loyalty;
+        this.honesty = honesty;
+    }
+
+    public int getLabour() {
+        return labour;
+    }
+
+    public void setLabour(int labour) {
+        this.labour = labour;
+    }
+
+    public int getLoyalty() {
+        return loyalty;
+    }
+
+    public void setLoyalty(int loyalty) {
+        this.loyalty = loyalty;
+    }
+
+    public int getHonesty() {
+        return honesty;
+    }
+
+    public void setHonesty(int honesty) {
+        this.honesty = honesty;
+    }
+}
