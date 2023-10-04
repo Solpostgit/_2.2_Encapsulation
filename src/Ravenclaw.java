5039cea20cf2d8e3b0686378e83f05ce06b2d730
Solpostgit <begemot_off@mail.ru>
2023-10-04 20:45:48 +0300

    Добавил класс Ravenclaw. Внёс поля и методы.

---
+++ src/Ravenclaw.java
@@ -0,0 +1,45 @@
+class Ravenclaw extends Hogwarts {
+    private int mind; //ум
+    private int wisdom; //мудрость
+    private int wit; //остроумие
+    private int creativity; //творчество
+    public Griffindor(String name, String faculty, int powerMagic, int transgresDist, int mind, int wisdom, int wit, int creativity) {
+        super(name, faculty, powerMagic, transgresDist);
+        this.mind = mind;
+        this.wisdom = wisdom;
+        this.wit = wit;
+        this.creativity = creativity;
+    }
+
+    public int getMind() {
+        return mind;
+    }
+
+    public void setMind(int mind) {
+        this.mind = mind;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getWit() {
+        return wit;
+    }
+
+    public void setWit(int wit) {
+        this.wit = wit;
+    }
+
+    public int getCreativity() {
+        return creativity;
+    }
+
+    public void setCreativity(int creativity) {
+        this.creativity = creativity;
+    }
+}
